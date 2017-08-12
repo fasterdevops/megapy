af70922662de0030243a295b3293d7ad24772435
Riley <riley@fasterdevops.com>
2017-08-12 19:01:26 -0400

    Updated arguments, cleared typos, and pep8/pylint cleaning.

--- megapy.py
+++ megapy.py
@@ -2,7 +2,6 @@
 '''
 MegaPy - MegaCLI wrapper
 '''
-import sh
 import os
 import subprocess
 import argparse
@@ -10,7 +9,7 @@ import argparse
 __version__ = 'MegaPy v.1'
 __author__ = 'Riley - riley@fasterdevops.com'
 
-MEGACLI_INSTALLATION_PATH = "/opt/MegaCli/"
+MEGACLI_INSTALLATION_PATH = "/opt/MegaRAID/"
 
 
 class MegaCLI(object):
@@ -33,7 +32,7 @@ class MegaCLI(object):
         # RedHat Command to install imh-megapkg
         install_megacli = ['/usr/bin/yum',
                            'install',
-                           'imh-megapkg',
+                           'megacli',
                            '-y']
         # If MegaCLI is not installed, prompt user
         # to see if they would like to install
@@ -91,8 +90,15 @@ class MegaCLI(object):
                                                '-PhysDrv',
                                                '[E:S]',
                                                '-aN']
+        self.disable_alarm_command = ['MegaCli64',
+                                      '-AdpSetProp',
+                                      'AlarmDsbl',
+                                      '-aALL']
+        self.enable_alarm_command = ['MegaCli64',
+                                     '-AdpSetProp',
+                                     'AlarmEnbl',
+                                     '-aALL']
 
-    # Using subprocess to to run megadir with command
     def view_enclosures(self):
         '''
         View the servers enclosure information
@@ -129,7 +135,7 @@ class MegaCLI(object):
 
     def stop_rebuild_drive(self, enclosure, slot):
         '''
-        View battery information
+        Stop drive rebuild
         :param enclosure - physical enclosure from view_enclosures()
         :param slot - slot of the enclosure
         '''
@@ -139,7 +145,7 @@ class MegaCLI(object):
 
     def show_rebuild_drive(self, enclosure, slot):
         '''
-        View battery information
+        Show drive rebuild information
         :param enclosure - physical enclosure from view_enclosures()
         :param slot - slot of the enclosure
         '''
@@ -149,7 +155,7 @@ class MegaCLI(object):
 
     def rebuild_drive(self, enclosure, slot):
         '''
-        View battery information
+        Rebuild the drive
         :param enclosure - physical enclosure from view_enclosures()
         :param slot - slot of the enclosure
         '''
@@ -164,38 +170,57 @@ class MegaCLI(object):
         '''
         self.megadir = os.path.dirname(megacli_dir)
 
+    def alarm_disable(self):
+        '''
+        Disable the alarm
+        '''
+        subprocess.Popen(self.megadir, self.disable_alarm_command)
+
+    def alarm_enable(self):
+        '''
+        Enable the alarm
+        '''
+        subprocess.Popen(self.megadir, self.enable_alarm_command)
+
 
 def parse_arguments():
     '''
     Parsing user arguments. Use -h to see available commands.
+    Had to add 'action='store_true' to arguments not expecting any input.
     '''
-    parser = argparse.ArgumentParser(description=('A wrapper to make '
-                                                  'MegaCLI commands more '
-                                                  'user friendly and easier '
-                                                  'to remember')
-                                    )
+    parser = argparse.ArgumentParser(description='A wrapper to make ' +
+                                     'MegaCLI commands more' +
+                                     'user friendly and easier' +
+                                     'to remember')
     parser.add_argument('--enclosure',
                         help='View the servers enclosure information',
-                        dest='encl'
-                       )
+                        dest='encl',
+                        action='store_true')
     parser.add_argument('--physical',
                         help='View the servers physical drive information',
-                        dest='phys'
-                       )
+                        action='store_true',
+                        dest='phys')
     parser.add_argument('--vdrive',
                         help='View the servers virtual drive information',
-                        dest='vdrive'
-                       )
+                        action='store_true',
+                        dest='vdrive')
     parser.add_argument('--controller',
                         help='View the servers controller information',
-                        dest='control'
-                       )
+                        action='store_true',
+                        dest='control')
+    parser.add_argument('--alarmoff',
+                        help='Turn the alarm off',
+                        action='store_true',
+                        dest='alarmoff')
+    parser.add_argument('--alarmon',
+                        action='store_true',
+                        dest='alarmon')
     parser.add_argument('--basedir',
                         help='Specify the base directory where' +
                         'MegaCLI is installed.',
-                        dest='basedir'
-                       )
-    return parser.parse_args()
+                        dest='basedir')
+    args = parser.parse_args()
+    return args
 
 
 def main():
@@ -206,23 +231,27 @@ def main():
 
     # display version and parse arguments
     print __version__
-    args = parse_arguments()
-
     # initialize MegaCLI object
     mega = MegaCLI()
     mega.check_install()
-
+    args = parse_arguments()
     if args.basedir:
         mega.set_basedir(args.basedir)
-
-    if args.encl:
-        mega.view_enclosures()
-    if args.vdrive:
-        mega.view_vdrive()
 
     if args.phys:
         mega.view_physical_drives()
 
+    if args.vdrive:
+        mega.view_vdrive()
+
+    if args.control:
+        mega.view_physical_drives()
+
+    if args.alarmoff:
+        mega.alarm_disable()
+
+    if args.alarmon:
+        mega.alarm_enable()
 
 if __name__ == '__main__':
     main()
